845b1a44b5a786e76f6da3df1989ce00d4f54c7b
Corah Chiu <corahchiu@gmail.com>
2017-09-19 23:02:42 +0800

    first commit

--- server.js
+++ server.js
@@ -56,31 +56,100 @@ app.get('/', function(req,res){
 
 // Search phrase in main language column
 app.post('/search', function(req, res){
+  // trying to get all the translated phrase in an array
   var phrase = req.body.phrase;
   var language = req.body.language;
+  var targetLanguage = req.body.targetLanguage;
+
+  // todo now I need to get the targetLanguage from the target language column
   session
-    .run(`MATCH (n {phrase:'${phrase}', language:'${language}'})-[:translation]->(b) RETURN n,b`)
-    .then(result => {
-      session.close();
-      let allPhrases = {};
-      // get original phrase
-      const singleRecord = result.records[0];      
-      const node = singleRecord.get(0);
-      allPhrases.main = node.properties.phrase;
-      allPhrases.target = [];
-      // get translated phrases
-      for (i=0; i<result.records.length; i++){    
-        const singleRecord = result.records[i];
-        const node = singleRecord.get(1);
-        allPhrases.target.push(node.properties.phrase);
-      }
-      console.log(allPhrases);      
-      res.send(allPhrases);
-    })  
-    .catch(function(err){
-      console.log(err);
-    })
+  .run(`MATCH (n {phrase:'${phrase}', language:'${language}'})-[:translation]->(b {language:'English'}) RETURN n,b`)  
+  // .run(`MATCH (n {phrase:'${phrase}', language:'${language}'})-[:translation]->(b {language:'${targetLanguage}'}) RETURN n,b`)
+  .then(result => {
+    session.close();
+    let allPhrases = {};
+    // get original phrase
+    const singleRecord = result.records[0];    
+    const mainNode = singleRecord.get(0);
+    allPhrases[mainNode.properties.language] = mainNode.properties.phrase;
+
+    for (i=0; i<result.records.length; i++){    
+            const singleRecord = result.records[i];
+            const targetNode = singleRecord.get(1);
+            allPhrases[targetNode.properties.language] = []    
+    }
+    for (i=0; i<result.records.length; i++){    
+      const singleRecord = result.records[i];
+      const targetNode = singleRecord.get(1);
+      allPhrases[targetNode.properties.language].push(targetNode.properties.phrase);
+    }
+
+    // allPhrases[targetNode.properties.language] = []
+    // allPhrases[targetNode.properties.language].push(targetNode.properties.phrase);
+    console.log(allPhrases);      
+    res.send(allPhrases); //What is being sent to service?
+  })  
+  .catch(function(err){
+    console.log(err);
+  })
+
+  // //result: { Hungarian: 'Szia', English: [ 'Hello' ] }
+  // var phrase = req.body.phrase;
+  // var language = req.body.language;
+  // var targetLanguage = req.body.targetLanguage;
+
+  // session
+  // .run(`MATCH (n {phrase:'${phrase}', language:'${language}'})-[:translation]->(b {language:'English'}) RETURN n,b`)  
+  // // .run(`MATCH (n {phrase:'${phrase}', language:'${language}'})-[:translation]->(b {language:'${targetLanguage}'}) RETURN n,b`)
+  // .then(result => {
+  //   session.close();
+  //   let allPhrases = {};
+  //   // get original phrase
+  //   const singleRecord = result.records[0];    
+  //   const mainNode = singleRecord.get(0);
+  //   const targetNode = singleRecord.get(1);
+  //   allPhrases[mainNode.properties.language] = mainNode.properties.phrase;
+  //   allPhrases[targetNode.properties.language] = []
+  //   allPhrases[targetNode.properties.language].push(targetNode.properties.phrase);
+  //   console.log(allPhrases);      
+  //   res.send(allPhrases); //What is being sent to service?
+  // })  
+  // .catch(function(err){
+  //   console.log(err);
+  // })
+
+
+  // // get all equivalent phrases
+  // var phrase = req.body.phrase;
+  // var language = req.body.language;
+  // session
+  //   .run(`MATCH (n {phrase:'${phrase}', language:'${language}'})-[:translation]->(b) RETURN n,b`)
+  //   .then(result => {
+  //     session.close();
+  //     let allPhrases = {};
+  //     // get original phrase
+  //     const singleRecord = result.records[0];      
+  //     const node = singleRecord.get(0);
+  //     allPhrases[node.properties.language] = node.properties.phrase;
+  //     // allPhrases.target = [];
+  //     // get translated phrases
+  //     for (i=0; i<result.records.length; i++){    
+  //       const singleRecord = result.records[i];
+  //       const node = singleRecord.get(1);
+  //       // allPhrases.target.push(node.properties.phrase);
+  //       allPhrases[node.properties.language] = node.properties.phrase;
+        
+  //     }
+  //     console.log(allPhrases);      
+  //     res.json(allPhrases);
+  //   })  
+  //   .catch(function(err){
+  //     console.log(err);
+  //   })
+
   // // get searched phrase
+  // var phrase = req.body.phrase;
+  // var language = req.body.language;
   // session
   //   .run(`MATCH (n {phrase:"${phrase}", language: "${language}"}) RETURN n`)
   //   .then(result => {
